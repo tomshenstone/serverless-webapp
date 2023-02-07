2bf62ca0520055c4480d20d2dd93fa5e0595e932
tomshenstone <tomshenstone@gmail.com>
2023-02-07 14:48:17 +0000

    new_config_js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-west-2_qFsj6nNDV', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6mhjqj88ah8kpp2u70ur05u4ad', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
